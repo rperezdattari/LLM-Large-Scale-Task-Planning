5d67f05efc6f6a6229039b8e0e9181f12f37ccab
ZhaotingLi <zhaoting_li@outlook.com>
2024-06-14 16:00:38 +0200

    5tasks: on_book_269 in env 3 always has issue, remove and resample tasks

--- experiments_info/tasks_dataset_5task.py
+++ experiments_info/tasks_dataset_5task.py
@@ -13,14 +13,6 @@
 #     {'goal': {0: {'on_milk_186': 1, 'on_whippedcream_189': 1, 'on_cutleryknife_189': 2, 'on_carrot_186': 1}}, 'id': 5},
 # ]
 
-# tasks = [
-#     {'goal': {0: {'inside_cutleryfork_104': 2, 'inside_waterglass_104': 1, 'inside_wineglass_104': 1, 'on_peach_72': 1}}, 'id': 3} ,
-#     {'goal': {0: {'inside_cutleryfork_104': 2, 'inside_plate_104': 1, 'inside_waterglass_104': 1, 'inside_wineglass_104': 1}}, 'id': 3} ,
-#     {'goal': {0: {'inside_apple_103': 1, 'inside_cutleryfork_104': 2, 'inside_waterglass_104': 2}}, 'id': 3} ,
-#     {'goal': {0: {'inside_apple_103': 2, 'inside_plate_104': 1, 'inside_wineglass_104': 1, 'on_crackers_72': 1}}, 'id': 3} ,
-#     {'goal': {0: {'inside_apple_103': 1, 'inside_cutleryfork_104': 1, 'inside_wineglass_104': 1, 'on_crackers_72': 1, 'on_peach_72': 1}}, 'id': 3} ,
-# ]
-
 
 tasks = [
     # {'goal': {0: {'inside_pie_305': 1, 'on_apple_111': 2, 'on_book_111': 1, 'on_cutleryfork_111': 1}}, 'id': 0} ,
@@ -38,11 +30,11 @@ tasks = [
     {'goal': {0: {'inside_book_158': 1, 'inside_cutleryknife_165': 2, 'inside_salmon_162': 1, 'turnon_163': 1}}, 'id': 2} ,
     # {'goal': {0: {'inside_cutleryfork_165': 2, 'inside_cutleryknife_165': 1, 'inside_milk_162': 1, 'on_cupcake_215': 1}}, 'id': 2} ,
     # {'goal': {0: {'inside_book_158': 1, 'inside_milk_162': 2, 'inside_plate_165': 1, 'turnon_163': 1}}, 'id': 2} ,
-    {'goal': {0: {'inside_apple_103': 1, 'inside_cutleryfork_104': 1, 'inside_wineglass_104': 1, 'on_book_269': 1, 'on_crackers_72': 1}}, 'id': 3} ,
-    {'goal': {0: {'inside_cutleryfork_104': 1, 'inside_waterglass_104': 1, 'on_book_269': 1, 'on_crackers_72': 1, 'on_peach_72': 1}}, 'id': 3} ,
-    {'goal': {0: {'inside_apple_103': 2, 'inside_wineglass_104': 1, 'on_book_269': 1, 'on_peach_72': 1}}, 'id': 3} ,
-    {'goal': {0: {'inside_apple_103': 2, 'inside_cutleryfork_104': 1, 'inside_wineglass_104': 1, 'on_book_269': 1}}, 'id': 3} ,
-    {'goal': {0: {'inside_apple_103': 2, 'inside_waterglass_104': 1, 'on_book_269': 1, 'on_peach_72': 1}}, 'id': 3} ,
+    {'goal': {0: {'inside_cutleryfork_104': 2, 'inside_waterglass_104': 1, 'inside_wineglass_104': 1, 'on_peach_72': 1}}, 'id': 3} ,
+    {'goal': {0: {'inside_cutleryfork_104': 2, 'inside_plate_104': 1, 'inside_waterglass_104': 1, 'inside_wineglass_104': 1}}, 'id': 3} ,
+    {'goal': {0: {'inside_apple_103': 1, 'inside_cutleryfork_104': 2, 'inside_waterglass_104': 2}}, 'id': 3} ,
+    {'goal': {0: {'inside_apple_103': 2, 'inside_plate_104': 1, 'inside_wineglass_104': 1, 'on_crackers_72': 1}}, 'id': 3} ,
+    {'goal': {0: {'inside_apple_103': 1, 'inside_cutleryfork_104': 1, 'inside_wineglass_104': 1, 'on_crackers_72': 1, 'on_peach_72': 1}}, 'id': 3} ,
     # {'goal': {0: {'inside_cupcake_157': 1, 'inside_mug_156': 1, 'on_breadslice_138': 1, 'on_cereal_138': 1, 'on_chocolatesyrup_138': 1}}, 'id': 4} ,
     # {'goal': {0: {'inside_pie_157': 1, 'inside_wineglass_156': 1, 'on_breadslice_138': 1, 'on_condimentbottle_138': 2}}, 'id': 4} ,
     # {'goal': {0: {'inside_cupcake_157': 1, 'inside_mincedmeat_157': 1, 'inside_wineglass_156': 2, 'on_chocolatesyrup_138': 1}}, 'id': 4} ,
